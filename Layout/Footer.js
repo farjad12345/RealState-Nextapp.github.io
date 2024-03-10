fe72ff9a11d66986a3d911e6151fdd2d844fea51
--F.B.M-- <72466068+farjad12345@users.noreply.github.com>
2024-03-10 13:54:43 +0330

    Update Footer.js

--- Layout/Footer.js
+++ Layout/Footer.js
@@ -5,18 +5,18 @@ const  Footer =()=> {
   return (
     <>
     <footer className=" flex flex-wrap text-center justify-betweentext-center justify-between" >
-    <ul class=" flex  justify-between">
+    <ul class=" flex  justify-between p-8">
                     <li class="list-none flex ">
-                        <a href="#" class=" text-white text-xl" >خانه رویایی خود را پیدا کنید</a >
+                        <a href="#" class=" text-white text-xl p-4" >خانه رویایی خود را پیدا کنید</a >
                     </li>
                     <li class="list-none text-sm">
-                        <a href="#" class=" text-white text-xl ">درخواست پروپوزال</a>
+                        <a href="#" class=" text-white text-xl  ">درخواست پروپوزال</a>
                     </li>
                     <li class="list-none text-sm">
-                        <a href="#" class=" text-white text-xl ">برنامه اجاره خانه ها</a>
+                        <a href="#" class=" text-white text-xl p-4">برنامه اجاره خانه ها</a>
                     </li>
                     <li class="list-none text-sm">
-                        <a href="#" class=" text-white text-xl">با ما تماس بگیرید</a>
+                        <a href="#" class=" text-white text-xl ">با ما تماس بگیرید</a>
                     </li>
                     <li class="list-none text-sm">
                         <a href="#" class=" text-white text-xl ">ملک خود را ارسال کنید</a>
