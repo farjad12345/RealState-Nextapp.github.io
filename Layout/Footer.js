2ebe670b7b5d17a7ca56d43defe2ab4f0eeb20f4
--F.B.M-- <72466068+farjad12345@users.noreply.github.com>
2024-03-10 13:35:40 +0330

    Update Footer.js

--- Layout/Footer.js
+++ Layout/Footer.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 const  Footer =()=> {  
   return (
     <>
-    <footer className="  bg-secondry flex-row text-center flex-wrap flex-wrap px-24" >
+    <footer className="  bg-secondry flex-row text-center flex-wrap  px-24" >
     <ul class=" flex  justify-between">
                     <li class="list-none flex j gap-4 ">
                         <a href="#" class=" text-white text-xl" >خانه رویایی خود را پیدا کنید</a >
